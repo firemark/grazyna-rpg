28dede89e1ef40bb7057bec97d8c23c15ab830e8
Firemark <marpiechula@gmail.com>
2015-07-13 01:41:47 +0200

    add db support on plugin

--- grazyna_rpg/plugin.py
+++ grazyna_rpg/plugin.py
@@ -1,10 +1,13 @@
-from grazyna.utils import register
+from grazyna.utils import register, init_plugin
 from grazyna.utils.event_loop import loop
+
+from grazyna_rpg.db import get_session, get_engine
 
 
 @init_plugin
 def on_load(plugin, config):
-    pass
+    engine = get_engine(config['db_uri'])
+    plugin.s = get_session(engine)
 
 
 @loop(time_config_key="tick_time", default=10)
