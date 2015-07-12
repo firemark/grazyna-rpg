cc62f50e1c4ccb75983de2758f835ace35211232
Firemark <marpiechula@gmail.com>
2015-07-12 23:18:09 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+requires = [
+    'grazyna>=0.5.2'
+]
+
+setup(
+    name='grazyna_rpg',
+    version='0.1',
+    description='RPG Mode',
+    long_description='RPG Mode',
+    classifiers=[],
+    author='Firemark',
+    author_email='marpiechula@gmail.com',
+    url='https://github.com/firemark/grazyna-rpg',
+    keywords='bot rpg grazyna'.split(),
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=requires,
+)
